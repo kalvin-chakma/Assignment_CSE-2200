b9ae3d79e7c5c5c5f1ac54c8d340bb54090716bf
kalvin-chakma <[kalvinchakma7@gmail.com]>
2024-05-18 23:45:31 +0600

    Implement Edit Todo Button

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ inputBar.addEventListener("keyup", function buttonStatus() {
     if (!saveButton.classList.contains("disabled")) {
       saveButton.classList.add("disabled");
     }
-  } else {
+    saveButton.classList.add("disabled");
+  } else if (saveButton.classList.contains("disabled")) {
     saveButton.classList.remove("disabled");
   }
 });
@@ -34,6 +35,28 @@ saveButton.addEventListener("click", function getTextAddTodo() {
   addTodo(todoText, todos.length - 1);
   inputBar.value = "";
 });
+
+function editTodo(event) {
+  let editeButtonPressed = event.target;
+  let indexToBeEdit = Number(editeButtonPressed.getAttribute("data-idx"));
+  let detailDiv = document.querySelector(`div[data-idx="${indexToBeEdit}"]`);
+  let input = document.querySelector(`input[data-idx="${indexToBeEdit}"]`);
+  detailDiv.style.display = "none";
+  input.type = "text";
+  input.value = detailDiv.textContent;
+}
+
+function saveEdit() {
+  let input = event.target;
+  let indexToBeEdit = Number(input.getAttribute("data-idx"));
+  let detailDiv = document.querySelector(`div[data-idx="${indexToBeEdit}"]`);
+  if (event.keyCode == 13) {
+    detailDiv.textContent = input.value;
+    detailDiv.style.display = "block";
+    input.value = "";
+    input.type = "hidden";
+  }
+}
 
 function removeAllTodos() {
   todoList.innerHTML = "";
@@ -62,6 +85,7 @@ function addTodo(todoData, index) {
   let hr = document.createElement("hr");
   let checkboxContainer = document.createElement("div");
   let checkbox = document.createElement("input");
+  let hiddenInput = document.createElement("input");
   let label = document.createElement("label");
 
   checkbox.type = "checkbox";
@@ -82,18 +106,25 @@ function addTodo(todoData, index) {
   );
   taskno.classList.add("todo-no", "text-dark", "font-weight-bold");
   taskDetails.classList.add("todo-detail", "text-muted");
+  hiddenInput.classList.add("hidden-input", "todo-detail");
   taskStatus.classList.add("todo-status", "text-muted");
   taskActions.classList.add("todo-actions", "mb-2");
   editbtn.classList.add("btn", "btn-success");
   deletebtn.classList.add("btn", "btn-danger", "mt-2");
 
   deletebtn.setAttribute("data-idx", index);
+  editbtn.setAttribute("data-idx", index);
+  taskDetails.setAttribute("data-idx", index);
+  hiddenInput.setAttribute("data-idx", index);
   deletebtn.onclick = removeTodo;
+  editbtn.onclick = editTodo;
+  hiddenInput.type = "hidden";
+  hiddenInput.addEventListener("keypress", saveEdit);
+  hiddenInput.addEventListener("click", saveEdit);
 
   taskno.textContent = index + 1;
   taskDetails.textContent = todoData;
   label.textContent = "Incomplete";
-
   editbtn.textContent = "Edit";
   deletebtn.textContent = "Delete";
 
@@ -101,6 +132,7 @@ function addTodo(todoData, index) {
   taskActions.appendChild(deletebtn);
   tasklist.appendChild(taskno);
   tasklist.appendChild(taskDetails);
+  tasklist.appendChild(hiddenInput);
   tasklist.appendChild(taskStatus);
   tasklist.appendChild(taskActions);
   rowdiv.appendChild(tasklist);
